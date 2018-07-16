0b4816fd37607a63e72b2c1e6a813bd9d35b7e8d
André Soares <770688@tce.ro.gov.br>
2018-07-16 12:09:07 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AnimeSorter
+{
+    class Program
+    {
+        public static string AnimeFolder = Directory.GetCurrentDirectory();
+        public static string DownloadedFolder = AnimeFolder.Remove(AnimeFolder.LastIndexOf('\\'));
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Moving Episodes...");
+            MoveEpisodes();
+            Console.WriteLine("Moving Folders...");
+            MoveFolders();
+            Console.WriteLine("Creating new folders...");
+            CreateNewAnimeFolders();
+            Console.WriteLine("Moving Episodes to Folders...");
+            MoveEpisodesToFolders();
+            Console.WriteLine("Renaming Episodes...");
+            RenameEpisodes();
+            Console.WriteLine("Ending...");
+        }
+
+        static void MoveEpisodes()
+        {
+            var listEpisodes = Directory.EnumerateFiles(DownloadedFolder).ToList();
+            var regex = new Regex("[0-9]");
+
+            foreach (var episode in listEpisodes)
+            {
+                var episodeName = episode.Substring(episode.LastIndexOf('\\') + 1);
+                if (regex.IsMatch(episodeName) && episodeName.Contains("[") && (episodeName.Contains(".mkv") || episodeName.Contains(".avi") || episodeName.Contains(".mp4")))
+                {
+                    Console.WriteLine("Moving " + episodeName.Remove(episodeName.LastIndexOf('.')) + " to the Anime folder");
+                    Directory.Move(DownloadedFolder + "\\" + episodeName, AnimeFolder + "\\" + episodeName);
+                }
+            }
+        }
+
+        static void MoveFolders()
+        {
+            var listFolders = Directory.EnumerateDirectories(DownloadedFolder).ToList();
+
+            foreach (var folder in listFolders)
+            {
+                var folderName = folder.Substring(folder.LastIndexOf('\\') + 1);
+
+                if (folderName.Contains("["))
+                {
+                    string entry;
+                    Console.WriteLine("Is " + folderName + " a anime folder? [Y,N]: ");
+
+                    do
+                    {
+                        entry = Console.ReadKey().Key.ToString();
+                        Console.WriteLine();
+                    } while (entry != "Y" && entry != "N");
+
+                    if (entry == "Y")
+                    {
+                        Console.WriteLine("Moving " + folderName + " to the Anime folder");
+                        Directory.Move(DownloadedFolder + "\\" + folderName, AnimeFolder + "\\" + folderName);
+                    }
+                }
+            }
+        }
+
+        static void CreateNewAnimeFolders()
+        {
+            var listEpisodes = Directory.EnumerateFiles(AnimeFolder).ToList();
+
+            foreach (var episode in listEpisodes)
+            {
+                var episodeName = episode.Substring(episode.LastIndexOf('\\') + 1);
+
+                if (!episode.Contains(".mkv") && !episode.Contains("mp4") && !episode.Contains("avi"))
+                {
+                    continue;
+                }
+
+                string folderName = hasFolder(episodeName);
+
+                if (folderName == "")
+                {
+                    folderName = episodeName.Substring(episodeName.LastIndexOf(']') + 2);
+                    folderName = folderName.Remove(folderName.LastIndexOf('-') - 1);
+                    Directory.CreateDirectory(AnimeFolder + "\\" + folderName);
+                }
+            }
+        }
+
+        static void MoveEpisodesToFolders()
+        {
+            var listEpisodes = Directory.EnumerateFiles(AnimeFolder).ToList();
+
+            foreach (var episode in listEpisodes)
+            {
+                var episodeName = episode.Substring(episode.LastIndexOf('\\') + 1);
+
+                if (!episode.Contains(".mkv") && !episode.Contains("mp4") && !episode.Contains("avi"))
+                {
+                    continue;
+                }
+
+                Directory.Move(AnimeFolder + "\\" + episodeName, AnimeFolder + "\\" + hasFolder(episodeName) + "\\" + episodeName);
+            }
+        }
+
+        static void RenameEpisodes()
+        {
+            var listFolders = Directory.EnumerateDirectories(AnimeFolder).ToList();
+
+            foreach (var folder in listFolders)
+            {
+                var listEpisodes = Directory.EnumerateFiles(folder).ToList();
+
+                foreach (var episode in listEpisodes)
+                {
+                    var episodeName = episode.Substring(episode.LastIndexOf('\\') + 1);
+
+                    if (!episode.Contains(".mkv") && !episode.Contains("mp4") && !episode.Contains("avi"))
+                    {
+                        continue;
+                    }
+
+                    var episodeNewName = episodeName.Substring(episodeName.LastIndexOf(']') + 2);
+                    episodeNewName = episodeNewName.Remove(episodeNewName.LastIndexOf('-') - 1);
+                    var episodeNumber = episodeName.Substring(episodeName.LastIndexOf('-') + 2, 2);
+                    var episodeExtension = episodeName.Substring(episodeName.LastIndexOf('.'));
+
+                    Directory.Move(folder + "\\" + episodeName, folder + "\\" + episodeNewName + " " + episodeNumber + episodeExtension);
+                }
+            }
+        }
+
+        static string hasFolder(string episodeName)
+        {
+            var listFolders = Directory.EnumerateDirectories(AnimeFolder).ToList();
+
+            foreach (var folder in listFolders)
+            {
+                var folderName = folder.Substring(folder.LastIndexOf('\\') + 1);
+                if (episodeName.Contains(folderName))
+                {
+                    return folderName;
+                }
+            }
+
+            return "";
+        }
+    }
+}
